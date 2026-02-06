8e6b6266518427b53d7152c7e8058f40069d57ff
Lorena <230255@fppiramide.com>
2026-02-06 09:12:04 +0100

    Primer commit

---
+++ vite-project/src/componentes/ButtonPrueba.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+// export default function ButtonPrueba() {
+
+// 	const [count, setCount] = useState(0)
+
+// 	return (
+//     	<button onClick={() => setCount((count) => count + 1)}>
+//     		count is {count}
+//     	</button>
+//   	)
+// }
+
+export default function ButtonPrueba({count,useState}) {
+
+	return (
+		<button>count is {count}</button>
+	)
+}
+
